21ef00abf6f545bd6fc92ffb931f8f3b5df1a938
rekahamburger <rekahamburger@gmail.com>
2022-12-01 16:52:15 +0100

    Javítás1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'egyeni-angular-project2-rekahamburger';
+  myimage: string = 'src/assets/images/abstract-art-background-with-acrylic-paint-stains-canvas-modern-art-color-texture_34050-535.jpg'
 }
